b67d69a9dbb7a97c9433afd87160d9070355f88e
Your Name <you@example.com>
2023-02-14 13:00:25 -0500

    update nav bar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+import navbars from "../json/nav.json"
 
 function Header(props) {
   return (
-    <div>
-      <h1>Header</h1>
-
+    <div class="headercontainer">
+      <nav class="navcontainer">
+          {navbars.map((navbar) =>  (
+              <Link to={navbar.link} target={navbar.target} class="navbar">{navbar.name}</Link>
+          ))}
+      </nav> 
+      <h1 class="mainheader">Sue Wirth's Portfolio</h1>
     </div>
   )
 }
